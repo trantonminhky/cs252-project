3d38d238b9e7328a2b93f5f8624c588f49295305
trantonminhky <fungusvalentinealt69420@gmail.com>
2025-11-19 23:40:37 +0700

    implemented routing for test profile

--- backend/src/routes/profileRoutes.js
+++ backend/src/routes/profileRoutes.js
@@ -1,3 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const profileController = require('')
+const profileController = require('../controllers/profileController');
+
+router.get('/test', profileController.test);
+
+module.exports = router;
