6e2742536661c4747d51e94c5f3526b67d0e88a2
trantonminhky <fungusvalentinealt69420@gmail.com>
2025-11-19 23:47:51 +0700

    PLEASE READ COMMENT IN profileServer.js

--- backend/src/services/profileService.js
+++ backend/src/services/profileService.js
@@ -1,6 +1,8 @@
 const axios = require('axios');
 const config = require('../config/config');
 
+// for some reason this only works on node even version number, i have no fucking idea why
+
 let db;
 import('enmap').then(async ({ default: Enmap }) => {
 	db = new Enmap({ name: 'db' });
