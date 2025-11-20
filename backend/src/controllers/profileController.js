2eca839c161ef93ba5796f8b8b5f72da18f969ea
trantonminhky <fungusvalentinealt69420@gmail.com>
2025-11-20 10:08:14 +0700

    implemented controller for test_get

--- backend/src/controllers/profileController.js
+++ backend/src/controllers/profileController.js
@@ -22,6 +22,24 @@ class ProfileController {
 			next(error);
 		}
 	}
+
+	async test_get(req, res, next) {
+		try {
+			const { user } = req.query;
+
+			if (!user) {
+				return res.status(400).json({
+					success: false,
+					error: { message: 'user is required' }
+				});
+			}
+
+			const response = await ProfileService.test_get(user);
+			res.json(response);
+		} catch (err) {
+			next(err);
+		}
+	}
 }
 
 module.exports = new ProfileController();
