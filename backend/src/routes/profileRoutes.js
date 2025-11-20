9d0eb7acc8418da06ed2515816e530ceac4588e0
trantonminhky <fungusvalentinealt69420@gmail.com>
2025-11-20 10:08:50 +0700

    routed test_get

--- backend/src/routes/profileRoutes.js
+++ backend/src/routes/profileRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const profileController = require('../controllers/profileController');
 
 router.get('/test-set', profileController.test_set);
+router.get('/test-get', profileController.test_get);
 
 module.exports = router;
