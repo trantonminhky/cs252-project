a5c81ba870d7f15dff04a1d56d07a3470077d1d7
trantonminhky <fungusvalentinealt69420@gmail.com>
2025-11-20 10:05:42 +0700

    implemented test_get for profileService

--- backend/src/services/profileService.js
+++ backend/src/services/profileService.js
@@ -16,6 +16,28 @@ class ProfileService {
 			throw new Error("failed");
 		}
 	}
+
+	async test_get(user) {
+		if (!user) {
+			throw new Error('User is required');
+		}
+
+		try {
+			const response = {}
+			if (!db.has(user)) {
+				response.success = false;
+				response.data = null;
+			} else {
+				const val = db.get(user);
+				response.success = true;
+				response.data = val;
+			}
+			
+			return response;
+		} catch (err) {
+			throw new Error("failed");
+		}
+	}
 }
 
 module.exports = new ProfileService();
