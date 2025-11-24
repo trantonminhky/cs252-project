59be556daefb122a26f4debaa792bbf0ae60ab8a
trantonminhky <fungusvalentinealt69420@gmail.com>
2025-11-25 06:49:00 +0700

    added second webhook parameter to bashes

--- backend/sendRequest.sh
+++ backend/sendRequest.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-MESSAGE="${1:-}"
+WEBHOOK="${1:-}"
+MESSAGE="${2:-}"
 
-if [[ -z "$MESSAGE" ]]; then
-  echo "Usage: $0 <message>"
+if [[ -z "$LINK" || -z "$MESSAGE" ]]; then
+  echo "Usage: $0 <link> <message>"
   exit 1
 fi
 
 curl -X POST \
   -H "Content-Type: application/json" \
   --data "{\"embeds\":[{\"title\":\"$MESSAGE\",\"color\":8421504,\"footer\":{\"text\":\"con chim\"}}]}" \
-  "https://discord.com/api/webhooks/1442462919337181354/eVviM40zdSo_kHaC8NC9Q5fsg91FIrCzJRpVTGpdmpaL7JLQNNozX1tFvkU3u8-OjsVl"
+  "$WEBHOOK"
