2f35042e247fe62afc727cca273af869bd4e48ca
trantonminhky <fungusvalentinealt69420@gmail.com>
2025-11-24 17:53:36 +0700

    added deleteTunnel.sh to announce tunnel deletion

---
+++ backend/deleteTunnel.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+curl -X POST \
+  -H "Content-Type: application/json" \
+  --data "{\"embeds\":[{\"title\":\"LAST TUNNEL DELETED\",\"color\":16711680,\"footer\":{\"text\":\"con cac\"}}]}" \
+  "https://discord.com/api/webhooks/1442462919337181354/eVviM40zdSo_kHaC8NC9Q5fsg91FIrCzJRpVTGpdmpaL7JLQNNozX1tFvkU3u8-OjsVl"
