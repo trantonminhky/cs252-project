3341092f8d1ca9a19fb122252cf61754b830e9a7
trantonminhky <fungusvalentinealt69420@gmail.com>
2025-11-25 16:10:43 +0700

    implemented routing for clear and clearAll

--- backend/src/routes/DBRoutes.js
+++ backend/src/routes/DBRoutes.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const DBController = require('../controllers/DBController');
 
+router.get('/clear', DBController.clear);
+router.get('/clearAll', DBController.clearAll);
 router.get('/export', DBController.export);
 router.get('/exportAll', DBController.exportAll);
 
