8e620034c504c33a6367537ba31de3e2a3af7870
trantonminhky <fungusvalentinealt69420@gmail.com>
2025-11-30 11:54:49 +0700

    implemented <AIController>.extractTags()

--- backend/src/controllers/AIController.js
+++ backend/src/controllers/AIController.js
@@ -30,6 +30,25 @@ class AIController {
 			next(error);
 		}
 	}
+
+	async extractTags(req, res, next) {
+		try {
+			const text = req.query.text;
+			if (!text) {
+				const response = new ServiceResponse(
+					false,
+					400,
+					"Text is required"
+				);
+				return res.status(response.statusCode).json(response.get());
+			}
+
+			const response = await AIService.extractTags(text);
+			res.status(response.statusCode).json(response.get());
+		} catch (err) {
+			next(err);
+		}
+	}
 }
 
 module.exports = new AIController();
