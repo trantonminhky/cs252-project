6815d27b0f86d57a81d735dc9e064c2bf7fb5dc0
trantonminhky <fungusvalentinealt69420@gmail.com>
2025-11-30 16:45:50 +0700

    implemented a database for locations

---
+++ backend/src/db/LocationDB.js
@@ -0,0 +1,54 @@
+class LocationDB {
+	constructor() {
+		import('enmap').then(async ({ default: Enmap }) => {
+			this.db = new Enmap({ name: 'LocationDB' });
+		});
+	}
+
+	set(key, val, path) {
+		try {
+			this.db.set(key, val, path);
+			// console.log(`SessionTokensDB set key=${key} val=${JSON.stringify(val)} success at path ${path}`);
+		} catch (err) {
+			console.error(err);
+		}
+	}
+
+	get(key, path) {
+		try {
+			const value = this.db.get(key, path);
+			// console.log(`SessionTokensDB get key=${key} returns ${value}`)
+			return value;
+		} catch (err) {
+			console.error(err);
+		}
+	}
+
+	delete(key, path) {
+		try {
+			this.db.delete(key, path);
+			// console.log(`SessionTokensDB delete key=${key}`);
+		} catch (err) {
+			console.error(err);
+		}
+	}
+
+	clear() {
+		try {
+			this.db.clear();
+		} catch (err) {
+			console.error(err);
+		}
+	}
+
+	export() {
+		try {
+			const exp = this.db.export();
+			return exp;
+		} catch (err) {
+			console.error(err);
+		}
+	}
+}
+
+module.exports = new LocationDB();
