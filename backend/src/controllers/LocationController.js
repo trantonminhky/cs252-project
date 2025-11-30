03acf639d0335cbf87c4d35e8bfc5fa1f27688fa
trantonminhky <fungusvalentinealt69420@gmail.com>
2025-11-30 17:49:27 +0700

    implemented <LocationController>.search() without tags

--- backend/src/controllers/LocationController.js
+++ backend/src/controllers/LocationController.js
@@ -1,9 +1,29 @@
+const ServiceResponse = require('../helper/ServiceResponse');
 const LocationService = require('../services/LocationService');
 
 class LocationController {
 	async importToDB(req, res, next) {
 		await LocationService.importToDB();
 		res.status(200).json("lol");
+	}
+
+	async search(req, res, next) {
+		try {
+			const { query } = req.query;
+			if (!query) {
+				const response = new ServiceResponse(
+					false,
+					200,
+					"Query is required"
+				);
+				return res.status(response.statusCode).json(response.get());
+			}
+
+			const response = await LocationService.search(query);
+			res.status(response.statusCode).json(response.get());
+		} catch (err) {
+			next(err);
+		}
 	}
 }
 
