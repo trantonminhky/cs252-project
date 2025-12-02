ba552127ff111976169897bd10b96c92881d8fbf
trantonminhky <fungusvalentinealt69420@gmail.com>
2025-12-03 00:14:30 +0700

    routed /import and imported data to DB

--- backend/src/routes/EventRoutes.js
+++ backend/src/routes/EventRoutes.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 const router = Router();
 import EventController from '../controllers/EventController.js';
 
+router.get('/import', EventController.importToDB);
 router.post('/create', EventController.createEvent);
 router.post('/subscribe', EventController.subscribe);
 router.get('/get-by-username', EventController.getByUsername);
