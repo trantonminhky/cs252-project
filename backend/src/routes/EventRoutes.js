f97a063a372dfefcfaeddf6b87f2ab72bfc9e39e
trantonminhky <fungusvalentinealt69420@gmail.com>
2025-12-03 01:28:08 +0700

    implemenented /unsubscribe and <EventDB>.delete()

--- backend/src/routes/EventRoutes.js
+++ backend/src/routes/EventRoutes.js
@@ -5,6 +5,7 @@ import EventController from '../controllers/EventController.js';
 router.get('/import', EventController.importToDB);
 router.post('/create', EventController.createEvent);
 router.post('/subscribe', EventController.subscribe);
+router.post('/unsubscribe', EventController.unsubscribe);
 router.get('/get-by-username', EventController.getByUsername);
 
 export default router;
