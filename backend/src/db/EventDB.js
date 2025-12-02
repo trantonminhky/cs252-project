95ccd330b5a1d42da43084c200b23f6675e45fd7
trantonminhky <fungusvalentinealt69420@gmail.com>
2025-12-02 19:24:12 +0700

    implemented eventDB and relevant manipulation functions

---
+++ backend/src/db/EventDB.js
@@ -0,0 +1,54 @@
+class EventDB {
+	constructor() {
+		import('enmap').then(async ({ default: Enmap }) => {
+			this.db = new Enmap({ name: 'EventDB' });
+		});
+	}
+
+	set(key, val, path) {
+		try {
+			this.db.set(key, val, path);
+			// console.log(`SessionTokensDB set key=${key} val=${JSON.stringify(val)} success at path ${path}`);
+		} catch (err) {
+			console.error(err);
+		}
+	}
+
+	get(key, path) {
+		try {
+			const value = this.db.get(key, path);
+			// console.log(`SessionTokensDB get key=${key} returns ${value}`)
+			return value;
+		} catch (err) {
+			console.error(err);
+		}
+	}
+
+	delete(key, path) {
+		try {
+			this.db.delete(key, path);
+			// console.log(`SessionTokensDB delete key=${key}`);
+		} catch (err) {
+			console.error(err);
+		}
+	}
+
+	clear() {
+		try {
+			this.db.clear();
+		} catch (err) {
+			console.error(err);
+		}
+	}
+
+	export() {
+		try {
+			const exp = this.db.export();
+			return exp;
+		} catch (err) {
+			console.error(err);
+		}
+	}
+}
+
+export default new EventDB();
