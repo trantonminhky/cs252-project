1452c9abcbc313191e59fe3b6abd4af22ce33313
trantonminhky <fungusvalentinealt69420@gmail.com>
2025-12-03 01:04:49 +0700

    implemented <AIService>.generateReviews()

--- backend/src/services/AIService.js
+++ backend/src/services/AIService.js
@@ -26,7 +26,7 @@ class AIService {
 		}
 
 		try {
-			const data = await gemini.ask(prompt, {model: model});
+			const data = await gemini.ask(prompt, { model: model });
 			const response = new ServiceResponse(
 				true,
 				200,
@@ -67,7 +67,37 @@ class AIService {
 			);
 			return response;
 		} catch (err) {
-			console.log(err);
+			console.error(err);
+			const response = new ServiceResponse(
+				false,
+				500,
+				"Something went wrong"
+			);
+			return response;
+		}
+	}
+
+	async generateReviews(place, model = 'gemini-flash-latest') {
+		if (!place) {
+			const response = new ServiceResponse(
+				false,
+				400,
+				"Place name is required"
+			);
+			return response;
+		}
+
+		try {
+			const data = await gemini.ask(`Provide a list of reviews for the place with name ${place} in JSON format. The JSON object data should have these fields: { String username, String id, String content, int rating, String date (formatted according to flutter DateTime format)}. The response should be a JSON array of review objects. Do not put it in codeblock of triple backtick, I want raw data that is easily parse-able`, { model: model });
+			const response = new ServiceResponse(
+				true,
+				200,
+				"Success",
+				JSON.parse(data)
+			)
+			return response;
+		} catch (err) {
+			console.error(err);
 			const response = new ServiceResponse(
 				false,
 				500,
