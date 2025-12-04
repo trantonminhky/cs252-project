04b72f73e8e1005bc018aa804ebd93917c182a6c
trantonminhky <fungusvalentinealt69420@gmail.com>
2025-12-04 23:15:21 +0700

    implemented CacheDB to store recent geocode results

---
+++ backend/src/db/CacheDB.js
@@ -0,0 +1,55 @@
+import Enmap from 'enmap';
+const CACHE_LIFETIME_MS = 86400000;
+
+class CacheDB {
+	constructor() {
+		this.db = new Enmap({ name: 'CacheDB' });
+	}
+
+	set(key, val, path) {
+		try {
+			this.db.set(key, val, path);
+			// console.log(`SessionTokensDB set key=${key} val=${JSON.stringify(val)} success at path ${path}`);
+		} catch (err) {
+			console.error(err);
+		}
+	}
+
+	get(key, path) {
+		try {
+			const value = this.db.get(key, path);
+			// console.log(`SessionTokensDB get key=${key} returns ${value}`)
+			return value;
+		} catch (err) {
+			console.error(err);
+		}
+	}
+
+	delete(key, path) {
+		try {
+			this.db.delete(key, path);
+			// console.log(`SessionTokensDB delete key=${key}`);
+		} catch (err) {
+			console.error(err);
+		}
+	}
+
+	clear() {
+		try {
+			this.db.clear();
+		} catch (err) {
+			console.error(err);
+		}
+	}
+
+	export() {
+		try {
+			const exp = this.db.export();
+			return exp;
+		} catch (err) {
+			console.error(err);
+		}
+	}
+}
+
+export default new CacheDB();
